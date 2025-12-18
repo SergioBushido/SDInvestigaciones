ec0004a9cfa8640069075e5744fba4b2624dd0c4
Sergio Fernández Jiménez <84178657+SergioBushido@users.noreply.github.com>
2025-12-18 13:56:21 +0000

    bug movil

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,19 @@
-/* src/components/Layout/Layout.jsx */
+import { createSignal } from 'solid-js';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import WhatsAppButton from '../WhatsAppButton/WhatsAppButton';
 
-const Layout = (props) => (
-  <>
-    <Header />
-    <main>{props.children}</main>
-    <Footer />
-    <WhatsAppButton />
-  </>
-);
+const Layout = (props) => {
+  const [isMenuOpen, setIsMenuOpen] = createSignal(false);
+
+  return (
+    <>
+      <Header isMenuOpen={isMenuOpen()} setIsMenuOpen={setIsMenuOpen} />
+      <main>{props.children}</main>
+      <Footer />
+      <WhatsAppButton />
+    </>
+  );
+};
 
 export default Layout;
